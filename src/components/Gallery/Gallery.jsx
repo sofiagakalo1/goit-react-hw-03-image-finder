62bd57ef50906bf478640e8d3b9f6c00b3859ace
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-01-27 16:49:12 +0100

    p

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,37 @@
-import {Component} from 'react';
+import { Component } from 'react';
+import axios from 'axios';
 
-class Gallery extends Component{
+class Gallery extends Component {
+  state = {
+    images: [],
+  };
+  render() {
+    return (
+      <div>
+        <header class="searchbar">
+          <form class="form">
+            <button type="submit" class="button">
+              <span class="button-label">Search</span>
+            </button>
+            <input
+              class="input"
+              type="text"
+              autocomplete="off"
+              autofocus
+              placeholder="Search images and photos"
+            />
+          </form>
+        </header>
+
+        <ul class="gallery">
+          <li class="gallery-item">
+            <img src="" alt="" />
+          </li>
+        </ul>
+        <button>Load more</button>
+      </div>
+    );
+  }
 }
 
 export default Gallery;
