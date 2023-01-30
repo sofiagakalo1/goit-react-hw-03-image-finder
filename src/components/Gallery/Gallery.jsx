3e83953479204f75510031c1f042c8138e1e3209
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-01-30 13:06:59 +0100

    update

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -5,6 +5,7 @@ import Searchbar from '../Searchbar';
 import ImageGallery from '../ImageGallery';
 import Modal from '../Modal';
 import Loader from '../Loader';
+import Button from '../Button';
 
 import { getGalleryItems } from '../services/galleryApi';
 
@@ -17,8 +18,8 @@ class Gallery extends Component {
     showModal: false,
     modalImg: '',
     page: 1,
-    totalPages: 1,
     perPage: 12,
+    totalHits: '',
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -33,7 +34,7 @@ class Gallery extends Component {
       const { search, page, perPage } = this.state;
       this.setState({ loading: true });
       const { hits, totalHits } = await getGalleryItems(search, page, perPage);
-      this.calcTotalPages(totalHits);
+      this.setState({ totalHits: totalHits });
       this.setState(({ items }) => ({ items: [...items, ...hits] }));
     } catch (error) {
       this.setState({ error: error.message });
@@ -58,30 +59,34 @@ class Gallery extends Component {
     this.setState({ showModal: false, modalImg: '' });
   };
 
-  calcTotalPages = totalHits => {
-    const { perPage } = this.state;
-    let pages = Math.floor(totalHits / 12);
-    pages = totalHits % perPage ? pages + 1 : pages;
-    console.log(pages);
-    this.setState({ totalPages: pages });
+  showLoadMore = () => {
+    const { totalHits, loading, items } = this.state;
+    if ((totalHits <= items.length && !loading) || items.length < 1) {
+      return Boolean;
+    }
   };
 
   render() {
     const { items, loading, error, modalImg, showModal, totalPages, page } =
       this.state;
-    const { openModal, closeModal, loadMore, seachImage } = this;
+    const { openModal, closeModal, loadMore, seachImage, showLoadMore } = this;
     return (
       <div className={css.Gallery}>
         <Searchbar onSubmit={seachImage} />
         {error && <p>{error}</p>}
         {loading && <p>Loading...</p>}
         <ImageGallery items={items} openModal={openModal} />
-        <Loader
-          loading={loading}
-          page={page}
-          totalPages={totalPages}
-          loadMore={loadMore}
-        />
+        {loading && (
+          <Loader
+            loading={loading}
+            page={page}
+            totalPages={totalPages}
+            loadMore={loadMore}
+          />
+        )}
+        {!showLoadMore() && (
+          <Button loadMore={loadMore} type="button" text="Load more" />
+        )}
         {showModal && <Modal close={closeModal} bigImg={modalImg} />}
       </div>
     );
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types';
 import { ThreeCircles } from 'react-loader-spinner';
-import Button from '../Button';
 
-const Loader = ({ page, totalPages, loading, loadMore }) => {
+const Loader = ({ loading }) => {
   if (loading) {
     return (
       <ThreeCircles
@@ -19,19 +18,10 @@ const Loader = ({ page, totalPages, loading, loadMore }) => {
       />
     );
   }
-  if (!totalPages) {
-    return <p>No matches. Try to find something else :(</p>;
-  }
-  if (page !== totalPages) {
-    return <Button type="button" text="Load more" loadMore={loadMore} />;
-  }
 };
 
 export default Loader;
 
 Loader.propTypes = {
-  page: PropTypes.number.isRequired,
-  totalPages: PropTypes.number.isRequired,
   loading: PropTypes.bool.isRequired,
-  loadMore: PropTypes.func.isRequired,
 };
